5962626a0b0b39c2f2aaa570bf4c9f2e38ece23c
Caitlin☼ <NbtK5PlPg79s>
2018-11-09 11:30:21 -0500

    AI picks a move from a set of equilvalent best moves (allows randomness)
    Also included: the AI will play against itself a specified number of games with depths varying between 1 and a specified upper limit

--- Agent.py
+++ Agent.py
@@ -9,7 +9,9 @@ from test.test_typechecks import Integer
 from tkinter.constants import CURRENT
 import time
 import copy
+import random
 from test.test_binop import isint
+from random import randint
 
 class TreeGenerator(object):
     
@@ -53,7 +55,7 @@ class TreeGenerator(object):
         tree = Node([0,0,0,0],[])   # create dummy node
         for move in Checkers.movesAvailable(Checkers(), board, self.player, False):  # for all moves available to make
             tree.addChild(self.fillSubTree(board, self.player, 1, move)) # fill their trees and add to the 
-        
+# TODO ~~~~~~~~~~~~~~~~~~~~ Doesn't need to do this
         return tree
     # end getTrees()
      
@@ -118,26 +120,30 @@ class AlphaBeta:
     
     # print utility value of root node (assuming it is max)
     # print names of all nodes visited during search
-    def __init__(self, depth, player,checkers):        
+    def __init__(self, depth, player):        
         self.depth = depth
         self.player = player
-        treeGen = TreeGenerator(checkers,player,depth)
         return
 
     def alpha_beta_search(self, node):
-        best_val = -self.infinity
+        best_val = -9999
         beta = self.infinity
 
         successors = node.getChildren()
-        best_state = None
+        best_states = None
         for state in successors:
             value = self.min_value(state, best_val, beta, self.depth)
-            if value > best_val:
-                best_val = value
-                best_state = state
+            if value == best_val:   # if this node is same value as current best node
+                best_states += [state]  # add it to the list
+            elif value > best_val:  # else if this node is better than current nodes
+                best_val = value        # wipe the slate
+                best_states = [state]
 #         print ("AlphaBeta:  Utility Value of Root Node: = " + str(best_val))
 #         print ("AlphaBeta:  Best State is: " + str(best_state.name))
-        return best_state   # returns a node
+        if best_states == None:  # if there were no possible states (e.g loses)
+            return Node([-1,-1,-1,-1],[])
+        else:
+            return best_states[random.randint(0,len(best_states)-1)]  # returns a random node of equally best value
 
     def max_value(self, node, alpha, beta, depth):
 #         print ("AlphaBeta-->MAX: Visited Node :: " + str(node.name))
@@ -149,6 +155,7 @@ class AlphaBeta:
         for state in successors:
             value = max(value, self.min_value(state, alpha, beta, depth-1))
             if value >= beta:
+                if beta == self.infinity: print("~~~~~~~~~ Value has an error: value = " + str(value))
                 return value
             alpha = max(alpha, value)
         return value
@@ -162,9 +169,10 @@ class AlphaBeta:
         successors = self.getSuccessors(node, self.player)
         for state in successors:
             value = min(value, self.max_value(state, alpha, beta, depth-1))
-            if value <= alpha:
-                return value
-            beta = min(beta, value)
+            if value <= alpha:  # if the current value is lower than the previous lowest value so far
+                if alpha == -self.infinity: print("~~~~~~~~~ Value has an error: value = " + str(value))
+                return value    # return it, it's now our chosen value
+            beta = min(beta, value) #otherwise set it as our lowest here (if it is) then keep going
 
         return value
     #                     #
@@ -184,10 +192,9 @@ class AlphaBeta:
 
     def getUtility(self, node):
         assert node is not None
-        return self.scoreNode(node)
+        return self.scoreNode(Checkers.movePiece(checkers, node.value, self.player*((-1)**(self.depth+1)), node.name, False, False))
     
-    def scoreNode(self, node):
-        board = Checkers.movePiece(checkers, node.value, self.player*((-1)**(self.depth+1)), node.name, False, False)
+    def scoreNode(self, board):
         score = 0
         
         for i in range(0, len(board)):
@@ -206,37 +213,108 @@ class AlphaBeta:
 if __name__ == "__main__":
     print("We're Agenting in here")
     
-    checkers = Checkers()
-    checkers.nextTurn
+    depth = 1
+    blackDepth = 2
+    redDepth = 2
+    turnMax = 100   # max # of turns
     
-    depth = 3
-# ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+    gameMax = 1
+    print('Will play %i games' % gameMax)
+    for redDepth in range(1,7):         # with this deep a red AI
+        for blackDepth in range(1,7):   # and  this deep a black AI 
+            for x in range(0,gameMax):  # play this many games
+                
+                checkers = Checkers()
+#                 checkers.nextTurn()    # uncomment for Black to go firsts
+                
+            # ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
+                print("\n\n\n")
+                print("AI will play full game of up to " + str(turnMax) + " moves")
+                print("Red depth = %i, Black depth = %i" % (redDepth,blackDepth))
+                red = AlphaBeta(redDepth,-1)
+                black = AlphaBeta(blackDepth,1)
+                
+                counter = 0     # counter for turns taken
+                concede = False # flag for an AI concedes
+                while checkers.nextTurn() and not concede and counter < turnMax:
+                    checkers.printBoard(checkers.board)
+                    player = checkers.whoseTurn()
+                    print("It is %s's turn" % player)
+                    if player == "R":
+                        depth = redDepth
+                    else:
+                        depth = blackDepth
+                        
+                    start_time = time.time()
+                    treeGen = TreeGenerator(copy.deepcopy(checkers), checkers.turn,depth)
+                    tree = treeGen.getTree(copy.deepcopy(checkers.board))
+                    print("~~~~~~~~ Time to generate potential moves: " + str(time.time() - start_time))
+                    start_time = time.time()
+                    best_move = None
+                    if player == "R":
+                        best_move = red.alpha_beta_search(tree)    # Find best move, save it as best_move
+                    else:
+                        best_move = black.alpha_beta_search(tree)    # Find best move, save it as best_move
+                    print("~~~~~~~~ Time to ABP through said tree: " + str(time.time() - start_time))
+                    print(str(best_move))
+                    if (best_move.name == [-1,-1,-1,-1]):
+                        concede = True
+                    else:
+                        checkers.board = checkers.movePiece(copy.deepcopy(checkers.board), checkers.turn, best_move.name, False, False)  # Make that move
+                              
+                        checkers.anyKings()
+                        counter += 1
+                        print("~~~~~~~~~~~~~~~ Next Turn (#%s)" % counter)  # signal it is the AI's turn
+                    
+                # end while     
+                
+                if concede:
+                    checkers.turn = checkers.turn*(-1)  
+                    print("Player %s has conceded !" %
+                                (('B' if (checkers.turn == 1) else 'R')))
+                elif counter >= turnMax: # if max # of turns reached
+                    print("No more moves allowed!")
+                    print("Player %s has the better board !" %   
+                            ('R' if (red.scoreNode(checkers.board) > black.scoreNode(checkers.board)) else 'B'))
+                else:   # if the game ended normally
+                    print("Player %s cannot make any moves !" % ('B' if (checkers.turn == 1) else 'R'))
+                
+                
+                
+                print("Player %s wins !" % ('R' if (checkers.turn == 1) else 'B'))
      
-    print("AI will play team B")
-    black = AlphaBeta(depth,1,checkers)
-       
-    while checkers.nextTurn():
-        checkers.printBoard(checkers.board)
-        print("It is %s's turn" % checkers.whoseTurn())
-        if checkers.whoseTurn() == "R":
-            checkers.board = checkers.getMove(copy.deepcopy(checkers.board),checkers.turn,False)
-        else:
-            start_time = time.time()
-            treeGen = TreeGenerator(copy.deepcopy(checkers), checkers.turn,depth)
-            tree = treeGen.getTree(copy.deepcopy(checkers.board))
-            print("~~~~~~~~ Time to generate init tree: " + str(time.time() - start_time))
-            start_time = time.time()
-            best_move = black.alpha_beta_search(tree)    # Find best move, save it as best_move
-            print("~~~~~~~~ Time to ABP through said tree: " + str(time.time() - start_time))
-            print(str(best_move))
-            checkers.board = checkers.movePiece(copy.deepcopy(checkers.board), checkers.turn, best_move.name, False, False)  # Make that move
-              
-        checkers.anyKings()
-        print("~~~~~~~~~~~~~~~ Next Turn")  # signal it is the AI's turn
-    # end while     
-       
-    print("Player %s cannot make any moves !" % ('B' if (checkers.turn == 1) else 'R'))
-    print("Player %s wins !" % ('R' if (checkers.turn == 1) else 'B'))
+     
+     
+     
+     
+     
+     
+     
+#     print("AI will play team B")
+#     black = AlphaBeta(depth,1,checkers)
+#        
+#     while checkers.nextTurn():
+#         checkers.printBoard(checkers.board)
+#         print("It is %s's turn" % checkers.whoseTurn())
+#         if checkers.whoseTurn() == "R":
+#             checkers.board = checkers.getMove(copy.deepcopy(checkers.board),checkers.turn,False)
+#         else:
+#             start_time = time.time()
+#             treeGen = TreeGenerator(copy.deepcopy(checkers), checkers.turn,depth)
+#             tree = treeGen.getTree(copy.deepcopy(checkers.board))
+#             print("~~~~~~~~ Time to generate potential moves: " + str(time.time() - start_time))
+#             start_time = time.time()
+#             best_move = black.alpha_beta_search(tree)    # Find best move, save it as best_move
+#             print("~~~~~~~~ Time to ABP through said tree: " + str(time.time() - start_time))
+#             print(str(best_move))
+#             checkers.board = checkers.movePiece(copy.deepcopy(checkers.board), checkers.turn, best_move.name, False, False)  # Make that move
+#               
+#         checkers.anyKings()
+#         print("~~~~~~~~~~~~~~~ Next Turn")  # signal it is the AI's turn
+#     # end while     
+#        
+#     print("Player %s cannot make any moves !" % ('B' if (checkers.turn == 1) else 'R'))
+#     print("Player %s wins !" % ('R' if (checkers.turn == 1) else 'B'))
     
     
       
